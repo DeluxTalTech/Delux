d8ac7755635c018b81da23c483b20fc148da774b
Maria-Louisa Põlder <mpolde@ttu.ee>
2020-05-04 16:22:12 +0300

    Domain, Facade, Infra, Pages -> Treatments testid (kõik ei tööta)+ IsDomain/Facade/Infra/PagesTested klassid + BaseTestRepositoryId klass + RepositoryTests

---
+++ Tests/Infra/Treatment/HairTreatmentsRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Delux.Data.Treatment;
+using Delux.Domain.Treatment;
+using Delux.Infra;
+using Delux.Infra.Common;
+using Delux.Infra.Treatment;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Delux.Tests.Infra.Treatment
+{
+    [TestClass]
+    public class HairTreatmentsRepositoryTests : RepositoryTests<HairTreatmentsRepository, HairTreatment, HairTreatmentData>
+    {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<SalonDbContext>()
+                //.UseInMemoryDatabase("TestDb")
+                .Options;
+            db = new SalonDbContext(options);
+            dbSet = ((SalonDbContext)db).HairTreatments;
+            obj = new HairTreatmentsRepository((SalonDbContext)db);
+            base.TestInitialize();
+        }
+
+        protected override Type GetBaseType() => typeof(IdRepository<HairTreatment, HairTreatmentData>);
+
+        protected override string GetId(HairTreatmentData d) => d.Id;
+
+        protected override HairTreatment GetObject(HairTreatmentData d) => new HairTreatment(d);
+
+        protected override void SetId(HairTreatmentData d, string id) => d.Id = id;
+    }
+}
---
+++ Tests/Pages/IsPagesTested.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Delux.Tests.Pages
+{
+    [TestClass]
+    public class IsPagesTested:AssemblyTests
+    {
+        private const string Assembly = "Delux.Pages";
+
+        protected override string Namespace(string name)
+        {
+            return $"{Assembly}.{name}";
+        }
+
+        [TestMethod]
+        public void IsExtensionsTested()
+        {
+            IsAllTested(Assembly, Namespace("Extensions"));
+        }
+
+        [TestMethod]
+        public void IsTechnicianTested()
+        {
+            IsAllTested(Assembly, Namespace("Technician"));
+        }
+        [TestMethod]
+        public void IsTreatmentTested()
+        {
+            IsAllTested(Assembly, Namespace("Treatment"));
+        }
+
+        [TestMethod]
+        public void IsTested()
+        {
+            IsAllTested(Assembly, base.Namespace("Pages"));
+        }
+    }
+}
