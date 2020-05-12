72588ddecb879bf679aa292fcda3a4159b824df2
Anni Mari Prees <anpree@ttu.ee>
2020-05-12 16:27:10 +0300

    Väiksed muudatused

--- Data/Appointment/AppointmentData.cs
+++ Data/Appointment/AppointmentData.cs
@@ -5,12 +5,13 @@ using Delux.Data.Common;
 
 namespace Delux.Data.Appointment
 {
-    public sealed class AppointmentData : IdData
+    public sealed class AppointmentData : ClientData
     {
         public DateTime? AppointmentDate { get; set; }
         public DateTime? AppointmentTime { get; set; }
-        public string ClientId { get; set; }
         public string TreatmentId { get; set; }
+        public DurationData Treatment { get; set; }
         public string TechnicianId { get; set; }
+        public WorkedYearsData Technician { get; set; }
     }
 }
---
+++ Data/Appointment/ClientData.cs
@@ -0,0 +1,11 @@
+﻿using Delux.Data.Common;
+
+namespace Delux.Data.Appointment
+{
+    public abstract class ClientData : IdData
+    {
+        public string ClientName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MailAddress { get; set; }
+    }
+}
