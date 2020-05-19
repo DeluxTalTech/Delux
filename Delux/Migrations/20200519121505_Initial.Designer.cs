32ebd67094097db08041b26ca2ff0959401bc841
carmenteearu@gmail.com <carmenteearu@gmail.com>
2020-05-19 15:49:29 +0300

    database update tehtud

--- Delux/Migrations/20200519121505_Initial.Designer.cs
+++ Delux/Migrations/20200519121505_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Delux.Delux.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20200519091702_Initial")]
+    [Migration("20200519121505_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
